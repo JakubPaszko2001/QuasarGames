2a7f3c999f10b87c230bd8dced9ca8e55e54d0bb
JakubPaszko2001 <jakubpaszko01@gmail.com>
2025-02-28 19:40:36 +0100

    Basics of website

---
+++ src/Components/Trailer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Trailer_Abyss from '../assets/Abyss-Trailer.mp4'
+const Trailer = () => {
+  return (
+    <div>
+        <video className='w-screen' autoPlay muted loop>
+            <source src={Trailer_Abyss} type="video/mp4" />
+        </video>
+    </div>
+  )
+}
+
+export default Trailer
---
+++ src/Pages/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Trailer from '../Components/Trailer'
+const Main = () => {
+  return (
+    <main>
+      <Navbar />
+      <Trailer />
+    </main>
+  )
+}
+
+export default Main
