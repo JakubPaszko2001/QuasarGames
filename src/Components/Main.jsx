f486868ba5bb3ad3834af1fc9ee6438257e19c7a
JakubPaszko2001 <jakubpaszko01@gmail.com>
2025-03-03 21:25:10 +0100

    bug

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import BackgroundImage from '../assets/Main_background.png'
+// import BackgroundImage from '../assets/Main_background.png'
 const Main = () => {
   return (
         <div className="h-2/3 w-screen flex justify-center items-center overflow-hidden">
-          <img className="w-[100%] h-[100%] object-cover" src={BackgroundImage} alt="Abyss Logo" />
+          {/* <img className="w-[100%] h-[100%] object-cover" src={BackgroundImage} alt="Abyss Logo" /> */}
         </div>
 
   )
