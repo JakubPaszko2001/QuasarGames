330c30c12873befd5c4f3400bfcd9cfe766328cd
JakubPaszko2001 <jakubpaszko01@gmail.com>
2025-03-03 20:47:29 +0100

    modified

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,7 +3,7 @@ import BackgroundImage from '../assets/Main_background.png'
 const Main = () => {
   return (
         <div className="h-2/3 w-screen flex justify-center items-center overflow-hidden">
-          <img className="w-[110%] h-[110%] object-cover" src={BackgroundImage} alt="Abyss Logo" />
+          <img className="w-[100%] h-[100%] object-cover" src={BackgroundImage} alt="Abyss Logo" />
         </div>
 
   )
