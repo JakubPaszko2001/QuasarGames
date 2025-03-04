8a5a1b98d2f63c56732b40c88126ec275984bceb
JakubPaszko2001 <jakubpaszko01@gmail.com>
2025-03-04 21:51:00 +0100

    chatGPT naprawa test

--- src/Components/Trailer.jsx
+++ src/Components/Trailer.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import Trailer_Abyss from '../assets/Trailer-Mobile.mp4'
+import React from 'react';
+import Trailer_Abyss from '../assets/Trailer-Mobile.mp4';
+
 const Trailer = () => {
   return (
-    <div className='resposive top-0 left-0 w-screen h-screen' >
-        <video className='w-screen h-screen object-cover' autoPlay muted loop playsInline>
-            <source src={Trailer_Abyss} type="video/mp4" />
-        </video>
-        <div className='absolute bottom-0 h-24 w-full bg-linear-to-t from-black from-10% to-transparent'></div>
+    <div className="relative inset-0 w-screen h-screen overflow-hidden">
+      <video
+        className="w-full h-full object-cover"
+        autoPlay
+        muted
+        loop
+        playsInline
+      >
+        <source src={Trailer_Abyss} type="video/mp4" />
+      </video>
+      <div className="absolute bottom-0 w-full h-24 bg-gradient-to-t from-black/80 to-transparent"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Trailer
+export default Trailer;
