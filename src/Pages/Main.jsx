ba6fa9256257ae6472672ed04b5928ffb2217fb5
JakubPaszko2001 <jakubpaszko01@gmail.com>
2025-03-31 22:49:44 +0200

    How to Play Section

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
-import Trailer from '../Components/Trailer'
+import Main_Page from '../Components/Main_Page'
 import Kick from '../Components/Kick'
-import QuickInfo from '../Components/QuickInfo'
 import GameElements from '../Components/GameElements'
-import HTP from '../Components/HTP'
-import TrailerAbyss from '../Components/TrailerAbyss'
+import Trailer_Section from '../Components/Trailer_Section'
+import How_To_Play_Section from '../Components/How_To_Play_Section'
 import Gallery from '../Components/Gallery'
 const Main = () => {
   return (
     <main>
       <Navbar />
-      <Trailer />
+      <Main_Page />
       <Kick />
       <GameElements />
-      <HTP />
-      {/* <TrailerAbyss /> */}
+      <Trailer_Section />
+      <How_To_Play_Section />
       {/* <Gallery /> */}
     </main>
   )
