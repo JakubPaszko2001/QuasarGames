16c4b23c2d8899d8a6b01632c8d66b5ecb094f26
JakubPaszko2001 <jakubpaszko01@gmail.com>
2025-04-06 21:26:26 +0200

    phone gallery section done

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Mousewheel } from 'swiper/modules'
 
@@ -7,23 +7,24 @@ import 'swiper/css/mousewheel'
 
 const images = [
   'https://scontent-waw2-2.xx.fbcdn.net/v/t1.15752-9/480942826_528294930307004_8430743161985563023_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=lVxEahmvK2wQ7kNvwF7cESQ&_nc_oc=Adm00mI8wF7wC7DWKAA8YkFsdqx1fWdokWqoeop2YwK7ETNCuyWN1moyaE8xXAgww4o&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-waw2-2.xx&oh=03_Q7cD2AHrFZOSUTLVxLc41EQ565QRNOhb7r2etHh8dFmk2EoF3Q&oe=681A1BE8',
+  'https://scontent-waw2-2.xx.fbcdn.net/v/t1.15752-9/484433045_1383213463009991_8171885387272722301_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=0zBUrh57yP4Q7kNvwG1DSVf&_nc_oc=AdklfYHnK988SvktmW8OuxTOUS3w1y86o7tw_XNjJKsrvG2XdmRA2Mr0g_mmkYpSmPA&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-waw2-2.xx&oh=03_Q7cD2AEYBUjuw6xjWuqkbRtTHrKviQVcJijMRxzsk7KMvjjwPA&oe=681A4B65',
   'https://scontent-waw2-2.xx.fbcdn.net/v/t1.15752-9/480942826_528294930307004_8430743161985563023_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=lVxEahmvK2wQ7kNvwF7cESQ&_nc_oc=Adm00mI8wF7wC7DWKAA8YkFsdqx1fWdokWqoeop2YwK7ETNCuyWN1moyaE8xXAgww4o&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-waw2-2.xx&oh=03_Q7cD2AHrFZOSUTLVxLc41EQ565QRNOhb7r2etHh8dFmk2EoF3Q&oe=681A1BE8',
+  'https://scontent-waw2-2.xx.fbcdn.net/v/t1.15752-9/484433045_1383213463009991_8171885387272722301_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=0zBUrh57yP4Q7kNvwG1DSVf&_nc_oc=AdklfYHnK988SvktmW8OuxTOUS3w1y86o7tw_XNjJKsrvG2XdmRA2Mr0g_mmkYpSmPA&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-waw2-2.xx&oh=03_Q7cD2AEYBUjuw6xjWuqkbRtTHrKviQVcJijMRxzsk7KMvjjwPA&oe=681A4B65',
   'https://scontent-waw2-2.xx.fbcdn.net/v/t1.15752-9/480942826_528294930307004_8430743161985563023_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=lVxEahmvK2wQ7kNvwF7cESQ&_nc_oc=Adm00mI8wF7wC7DWKAA8YkFsdqx1fWdokWqoeop2YwK7ETNCuyWN1moyaE8xXAgww4o&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-waw2-2.xx&oh=03_Q7cD2AHrFZOSUTLVxLc41EQ565QRNOhb7r2etHh8dFmk2EoF3Q&oe=681A1BE8',
-  'https://scontent-waw2-2.xx.fbcdn.net/v/t1.15752-9/480942826_528294930307004_8430743161985563023_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=lVxEahmvK2wQ7kNvwF7cESQ&_nc_oc=Adm00mI8wF7wC7DWKAA8YkFsdqx1fWdokWqoeop2YwK7ETNCuyWN1moyaE8xXAgww4o&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-waw2-2.xx&oh=03_Q7cD2AHrFZOSUTLVxLc41EQ565QRNOhb7r2etHh8dFmk2EoF3Q&oe=681A1BE8',
-  'https://scontent-waw2-2.xx.fbcdn.net/v/t1.15752-9/480942826_528294930307004_8430743161985563023_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=lVxEahmvK2wQ7kNvwF7cESQ&_nc_oc=Adm00mI8wF7wC7DWKAA8YkFsdqx1fWdokWqoeop2YwK7ETNCuyWN1moyaE8xXAgww4o&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-waw2-2.xx&oh=03_Q7cD2AHrFZOSUTLVxLc41EQ565QRNOhb7r2etHh8dFmk2EoF3Q&oe=681A1BE8',
-  'https://scontent-waw2-2.xx.fbcdn.net/v/t1.15752-9/480942826_528294930307004_8430743161985563023_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=lVxEahmvK2wQ7kNvwF7cESQ&_nc_oc=Adm00mI8wF7wC7DWKAA8YkFsdqx1fWdokWqoeop2YwK7ETNCuyWN1moyaE8xXAgww4o&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-waw2-2.xx&oh=03_Q7cD2AHrFZOSUTLVxLc41EQ565QRNOhb7r2etHh8dFmk2EoF3Q&oe=681A1BE8',
-  'https://scontent-waw2-2.xx.fbcdn.net/v/t1.15752-9/480942826_528294930307004_8430743161985563023_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=lVxEahmvK2wQ7kNvwF7cESQ&_nc_oc=Adm00mI8wF7wC7DWKAA8YkFsdqx1fWdokWqoeop2YwK7ETNCuyWN1moyaE8xXAgww4o&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-waw2-2.xx&oh=03_Q7cD2AHrFZOSUTLVxLc41EQ565QRNOhb7r2etHh8dFmk2EoF3Q&oe=681A1BE8',
+  'https://scontent-waw2-2.xx.fbcdn.net/v/t1.15752-9/484433045_1383213463009991_8171885387272722301_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=0zBUrh57yP4Q7kNvwG1DSVf&_nc_oc=AdklfYHnK988SvktmW8OuxTOUS3w1y86o7tw_XNjJKsrvG2XdmRA2Mr0g_mmkYpSmPA&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-waw2-2.xx&oh=03_Q7cD2AEYBUjuw6xjWuqkbRtTHrKviQVcJijMRxzsk7KMvjjwPA&oe=681A4B65',
 ]
 
 const Gallery = () => {
+  const [selectedImage, setSelectedImage] = useState(null)
+
   return (
     <div className="w-full h-screen bg-black flex flex-col">
-      {/* Tytuł */}
+      {/* Header */}
       <div className="w-full h-1/5 flex justify-center items-center">
         <h1 className="text-5xl text-[#b9935b] kranky-regular">Gallery</h1>
       </div>
 
-      {/* Swiper z 3 widocznymi zdjęciami */}
+      {/* Swiper */}
       <div className="h-4/5 px-10">
         <Swiper
           direction="vertical"
@@ -39,43 +40,96 @@ const Gallery = () => {
             <SwiperSlide
               key={index}
               className="flex justify-center items-center transition-all duration-700 ease-in-out"
+              onClick={() => setSelectedImage(src)}
             >
               <img
                 src={src}
                 alt={`slide-${index}`}
-                className="w-full h-full object-cover rounded-3xl swiper-slide-img"
+                className="w-full h-full object-cover rounded-2xl cursor-pointer"
               />
             </SwiperSlide>
           ))}
         </Swiper>
       </div>
 
-      {/* Style efektów aktywnego slajdu */}
-<style jsx="true">{`
-  .swiper-slide {
-    filter: blur(2px);
-    opacity: 0.6;
-    scale: 0.85;
-    transition: all 0.5s ease;
-  }
+      {/* Modal */}
+      {selectedImage !== null && (
+  <div
+    className="fixed inset-0 bg-black/80 backdrop-blur-sm flex justify-center items-center z-50 px-4"
+    onClick={() => setSelectedImage(null)}
+  >
+    {/* Zamknij */}
+    <button
+      onClick={() => setSelectedImage(null)}
+      className="absolute top-4 right-4 text-white text-3xl bg-black/60 rounded-full p-2 hover:bg-black/80 z-50"
+    >
+      ✕
+    </button>
 
-  .swiper-slide-prev {
-    margin-bottom: -50px;
-  }
+    {/* Poprzednie zdjęcie */}
+    <button
+      onClick={(e) => {
+        e.stopPropagation()
+        const currentIndex = images.indexOf(selectedImage)
+        const prevIndex = (currentIndex - 1 + images.length) % images.length
+        setSelectedImage(images[prevIndex])
+      }}
+      className="absolute left-4 top-1/2 -translate-y-1/2 text-white text-3xl z-50"
+    >
+      ←
+    </button>
 
-  .swiper-slide-next {
-    margin-top: -50px;
-  }
+    {/* Następne zdjęcie */}
+    <button
+      onClick={(e) => {
+        e.stopPropagation()
+        const currentIndex = images.indexOf(selectedImage)
+        const nextIndex = (currentIndex + 1) % images.length
+        setSelectedImage(images[nextIndex])
+      }}
+      className="absolute right-4 top-1/2 -translate-y-1/2 text-white text-3xl z-50"
+    >
+      →
+    </button>
 
-  .swiper-slide-active {
-    filter: blur(0px);
-    opacity: 1;
-    scale: 1;
-    margin: 0;
-    z-index: 10;
-  }
-`}</style>
+    {/* Obraz */}
+    <div
+      className="relative w-full h-full flex justify-center items-center"
+      onClick={(e) => e.stopPropagation()}
+    >
+      <img
+        src={selectedImage}
+        alt="modal"
+        className="max-h-[90vh] max-w-full object-contain rounded-xl shadow-xl"
+      />
+    </div>
+  </div>
+)}
+      {/* Styl efektów slajdów */}
+      <style jsx="true">{`
+        .swiper-slide {
+          filter: blur(2px);
+          opacity: 0.6;
+          scale: 0.85;
+          transition: all 0.5s ease;
+        }
 
+        .swiper-slide-prev {
+          margin-bottom: -50px;
+        }
+
+        .swiper-slide-next {
+          margin-top: -50px;
+        }
+
+        .swiper-slide-active {
+          filter: blur(0px);
+          opacity: 1;
+          scale: 1;
+          margin: 0;
+          z-index: 10;
+        }
+      `}</style>
     </div>
   )
 }
