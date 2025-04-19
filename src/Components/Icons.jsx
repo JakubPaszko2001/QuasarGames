64fc859ae81d044533d6f84920cf084a38ddaedb
JakubPaszko2001 <jakubpaszko01@gmail.com>
2025-04-20 00:14:56 +0200

    quasarLogo

--- src/Components/Icons.jsx
+++ src/Components/Icons.jsx
@@ -3,7 +3,6 @@ import { BsLinkedin, BsGithub } from "react-icons/bs";
 import { ImSun } from "react-icons/im";
 import { TbMoon } from "react-icons/tb";
 import Logo from "../assets/Abyss-Logo2.webp";
-import LogoAbyss from "../assets/Abyss-Logo-Napis.webp";
 const Icons = ({ menuOpen }) => {
   return (
     <div className="flex items-center text-4xl gap-4 md:gap-2">
